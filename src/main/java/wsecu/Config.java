7c6e0f71eac41c31c37bb0c86897e5d5543ddaf6
Bill Homan <bhoman01@gmail.com>
2019-06-04 20:51:09 -0700

    Missed Config file on previous checkin

--- src/main/java/wsecu/Config.java
+++ src/main/java/wsecu/Config.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
